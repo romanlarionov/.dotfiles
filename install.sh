27498c8d6fc6abb15d8a5fc429f8b43ed2f5643c
Roman Larionov <romanlarionov@Romans-MacBook-Pro.local>
2014-07-08 20:45:57 -0400

    Added install script and moved dotfiles to ~/.dotfiles directory.

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+############################
+# install.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/.dotfiles
+############################
+
+########## Variables
+
+dir=~/.dotfiles                    # dotfiles directory
+files=".vimrc .vim .zshrc .oh-my-zsh .fonts .gitconfig .iterm "    # list of files/folders to symlink in homedir
+
+##########
+
+# Change to the dotfiles directory.
+echo -n "Changing to the dotfiles directory ..."
+cd $dir
+echo "done"
+
+# Move any existing dotfiles in ~/ to ~/.dotfiles, then create symlinks from ~/ to any files in ~/.dotfiles specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/.dotfiles/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/.$file ~/.$file
+done
+
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d $dir/.oh-my-zsh/ ]]; then
+        git clone http://github.com/robbyrussell/oh-my-zsh.git
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        sudo apt-get install zsh
+        install_zsh
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh
