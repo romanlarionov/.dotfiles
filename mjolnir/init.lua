37fd01b6b0b9dc71005383c86b1f0f279b677f19
Roman Larionov <romanylarionov@gmail.com>
2014-09-16 13:42:42 -0400

    Updated mjolnir config with spotify support.

--- mjolnir/init.lua
+++ mjolnir/init.lua
@@ -1,5 +1,58 @@
+
+---
+-- Written By: Roman Larionov
+-- File Name: init.lua
+--
+-- Description: This is a config file for Roman Larionov's personal 
+-- mjolnir window manager. This has bindings for custom hotkeys, 
+-- spotify control, alerts, window grids, etc.
+--
+-- This file needs to be placed within ~/.mjolnir.
+-- This will be auto loaded whenever Mjolnir is run.
+---
+
+
+------------ External Dependencies. 
 local application = require "mjolnir.application"
 local hotkey = require "mjolnir.hotkey"
 local window = require "mjolnir.window"
+local grid = require "mjolnir.sd.grid"
+--local fnutils = require "mjolnir.fnutils"
 local spotify = require "mjolnir.lb.spotify"
+local alert = require "mjolnir.alert"
+
+
+----------- Global Variables
+local mash = {"cmd", "alt", "ctrl"}
+local mashShift = {"cmd", "alt", "shift"}
+
+local function reloadConfig()
+	mjolnir.reload()
+	alert.show("Config Reloaded!")
+end
+
+----------- Custom Hotkeys
+
+-- Spotify 
+hotkey.bind(mash, "space", spotify.displayCurrentTrack)
+hotkey.bind(mash, "p", spotify.play) -- Play/Pause
+hotkey.bind(mash, "right", spotify.next)
+hotkey.bind(mash, "left", spotify.previous)
+
+-- Window Management 
+hotkey.bind(mash, "k", grid.pushwindow_left)
+hotkey.bind(mash, "l", grid.pushwindow_right)
+
+hotkey.bind(mash, "m", window.maximize)
+hotkey.bind(mash, "n", window.minimize)
+
+-- Mjolnir Specific
+hotkey.bind(mash, "r", reloadConfig)
+
+-- On StartUp
+
+
+
+
+
 
