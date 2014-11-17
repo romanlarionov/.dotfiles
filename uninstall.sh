a2e823bdd1c8bc8f9181563ffa4f6d4e81fc4fe7
Roman Larionov <romanylarionov@gmail.com>
2014-11-17 01:01:16 -0500

    Added xcode vimrc file, removed clion vimrc plugin dependencies.

--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@
 ########## Variables
 
 dir=~/.dotfiles
-files= "vimrc ideavimrc tmux.conf vim oh-my-zsh zshrc gitconfig mjolnir"
+files= "vimrc ideavimrc xvimrc tmux.conf vim oh-my-zsh zshrc gitconfig mjolnir"
 oldFiles=$dir/.dotfiles.old
 
 ########## Deletions
