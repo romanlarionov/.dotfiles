1586009099d6174de4ea022df44aa30f892d4c1f
Roman Larionov <romanylarionov@gmail.com>
2017-11-27 22:50:36 -0800

    ACTUALLY fixed spacevim issues. Seems like they completely changed everything since last time I played with it.

--- install.sh
+++ install.sh
@@ -32,41 +32,25 @@ cd ${dotfilesDir}
 if [[ "$(lsb_release -si)" == "Ubuntu" ]]; then
     # Useful tools and libraries
     sudo apt-get install -y zsh cmake wget curl
-    sudo apt install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
-                        libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-                        libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-                        python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
-
-    # Building vim from source
-    sudo apt remove vim vim-runtime gvim
-    cd ${HOME}
-    git clone https://github.com/vim/vim.git
-    cd vim
-    ./configure --with-features=huge \
-                --enable-multibyte \
-                --enable-rubyinterp=yes \
-                --enable-pythoninterp=yes \
-                --with-python-config-dir=/usr/lib/python2.7/config \
-                --enable-python3interp=yes \
-                --with-python3-config-dir=/usr/lib/python3.5/config \
-                --enable-perlinterp=yes \
-                --enable-luainterp=yes \
-                --enable-gui=gtk2 \
-                --enable-cscope \
-                --prefix=/usr/local
-    make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
-    sudo make install
-
-    sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-    sudo update-alternatives --set editor /usr/bin/vim
-    sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-    sudo update-alternatives --set vi /usr/bin/vim
-    alias vim=/usr/local/bin/vim # might be incorrect sometimes
+    
+    # replacing vim with neovim
+    sudo apt remove vim
+    sudo apt-get install -y software-properties-common python-software-properties
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
+    sudo apt-get install -y neovim python-dev python-pip python3-dev python3-pip python3-setuptools
+    sudo easy_install3 pip
+    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+    sudo update-alternatives --config vim
+    sudo pip install --upgrade neovim
+    sudo pip2 install --upgrade neovim
+    gem install neovim
 
     # Powerline fonts
-    git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git .fonts/ubuntu
+    git clone https://github.com/pdf/ubuntu-mono-powerline-ttf.git ${dotfilesDir}/.fonts/ubuntu
     cd ${dotfilesDir}/.fonts/ubuntu
-    sudo cp -f *.ttf /usr/share/fonts
+    sudo cp -f *.ttf /usr/share/fonts/truetype
+    sudo cp -f *.otf /usr/share/fonts/opentype
     fc-cache -vf
     cd ${dotfilesDir}
 fi
@@ -82,12 +66,15 @@ fi
 ########## SpaceVim
 curl -sLf https://spacevim.org/install.sh | bash
 mkdir ~/.SpaceVim.d/
-ln -s ${dotfilesDir}/init.vim ~/.SpaceVim.d/init.vim
+cp ${dotfilesDir}/init.vim ~/.SpaceVim.d/init.vim
 
 ########## Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
+echo ""
+echo ""
+echo ""
 echo "done"
 echo "NOTE: ZSH requires reboot for complete installation"
 echo -n "Do it now? (y/n): "
