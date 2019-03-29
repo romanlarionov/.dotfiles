7439d67c5bdd7458ab7c61c2a0f77016248273ce
Roman Larionov <romanylarionov@gmail.com>
2019-03-29 11:03:10 -0700

    Added fix for spacevim on mingw.

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 DOTFILES_DIR="${HOME}/.dotfiles"
-FILES=".SpaceVim.d .bashrc .zshrc .gitconfig .fonts .tmux.conf .clang-format" # list of files/folders to symlink in homedir
+FILES=".SpaceVim.d .bashrc .zshrc .minttyrc .gitconfig .fonts .tmux.conf .clang-format" # list of files/folders to symlink in homedir
+
+if [[ "$(expr substr $(uname -s) 1 10)" != "MINGW64_NT" ]]; then
+    read -p "Do you want to install zsh? (y/n): " -n 1 -r ZSH_REPLY
+    echo
+fi
+
+read -p "Do you want to install Spacevim? (y/n): " -n 1 -r SPACEVIM_REPLY
+echo
 
 # File Management
-read -p "Do you want to install zsh? (y/n): " -n 1 -r ZSH_ANSWER
-echo
-read -p "Do you want to install Spacevim? (y/n): " -n 1 -r SPACEVIM_ANSWER
-echo
 
 if [[ ! -d ${DOTFILES_DIR}/OldDotFiles ]]; then
     mkdir ${DOTFILES_DIR}/OldDotFiles
@@ -28,7 +32,7 @@ fi
 if [[ "$(lsb_release -si 2>/dev/null)" == "Ubuntu" ]]; then
     sudo apt-get install -q -y cmake wget curl clang-format cscope xfonts-utils
 
-    if [[ $ZSH_REPLY =~ ^[Yy]$ ]]; then
+    if [[ ${ZSH_REPLY} =~ ^[Yy]$ ]]; then
         sudo apt-get install -q -y zsh
     fi
     
@@ -47,7 +51,7 @@ if [[ "${uname}" == "Darwin" ]]; then
         brew install git brew-cask cmake wget
         brew linkapps
 
-        if [[ $ZSH_REPLY =~ ^[Yy]$ ]]; then
+        if [[ ${ZSH_REPLY} =~ ^[Yy]$ ]]; then
             brew install zsh
         fi
     else
@@ -55,11 +59,22 @@ if [[ "${uname}" == "Darwin" ]]; then
     fi
 fi
     
-if [[ $SPACEVIM_ANSWER =~ ^[Yy]$ ]]; then
+if [[ ${SPACEVIM_REPLY} =~ ^[Yy]$ ]]; then
     curl -sLf https://spacevim.org/install.sh | bash
+
+    # MinGW on windows fails to install some SpaceVim packages
+    if [[ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]]; then
+        URLS=$(curl -s https://api.github.com/repos/Shougo/vimproc.vim/releases/latest | grep "browser_download_url" | cut -d \" -f 4)
+        for URL in ${URLS}; do
+            file=$(echo ${URL} | cut -d '/' -f 9)
+            curl -sLf# -o ${HOME}/.cache/vimfiles/repos/github.com/Shougo/vimproc.vim/lib/${file} ${URL}
+        done
+
+        # todo: clang needs to be install for NeoFormat to work
+    fi
 fi
 
-if [[ $ZSH_REPLY =~ ^[Yy]$ ]]; then
+if [[ ${ZSH_REPLY} =~ ^[Yy]$ ]]; then
     if [[ "${SHELL}" != "zsh" ]]; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null
 
@@ -67,7 +82,7 @@ if [[ $ZSH_REPLY =~ ^[Yy]$ ]]; then
             chsh -s $(which zsh)
 
             read -p "ZSH requires reboot for complete installation. Do it now? (y/n): " -n 1 -r
-            if [[ $ZSH_REPLY =~ ^[Yy]$ ]]; then
+            if [[ ${ZSH_REPLY} =~ ^[Yy]$ ]]; then
             	sudo shutdown -r now
             fi
         else
