58b0865208c47fbca50610f08b2406f293f67748
Roman Larionov <romanylarionov@gmail.com>
2020-09-18 20:52:21 -0700

    update

---
+++ check_diff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Check if the dotfiles located within the $HOME directory contains any
+# differences from their counterparts in the $HOME/.dotfiles directory.
+#
+
+DOTFILES_DIR="${HOME}/.dotfiles"
+source ${DOTFILES_DIR}/.files
+
+for file in ${FILES}; do
+    if [[ -f  ${HOME}/${file} || -d ${HOME}/${file} ]]; then
+
+        # only move the files if they aren't already symlinked to the .dotfiles dir
+        if [[ ! -L ${HOME}/${file} ]]; then
+
+            if [[ ! -z "$(diff ${HOME}/${file} ${file})" ]]; then
+                diff -q ${HOME}/${file} ${DOTFILES_DIR}/${file}
+            fi
+        fi
+    fi
+done
+
